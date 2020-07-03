9700e0fcd0c78b69cfe96d641d525f9b9e01f762
Alex <alexsan227@gmail.com>
2020-07-03 17:00:57 -0300

    Update database name

--- server/config/config.js
+++ server/config/config.js
@@ -15,5 +15,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 if (process.env.NODE_ENV === 'dev') {
     process.env.URL_DB = 'mongodb://localhost:27017/RaycrusCoffeDB'
 } else {
-    process.env.URL_DB = 'mongodb+srv://admin:4ujTPCQsxB5pV9y4@cluster0.lhkhu.mongodb.net/<dbname>?retryWrites=true&w=majority'
+    process.env.URL_DB = 'mongodb+srv://admin:4ujTPCQsxB5pV9y4@cluster0.lhkhu.mongodb.net/RaycrusCoffeDB?retryWrites=true&w=majority'
 }
